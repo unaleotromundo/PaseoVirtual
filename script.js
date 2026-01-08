a5a95fee3d7ef28267b484686bc753a6c43f1563
aleotromundo <contralamaquina@gmail.com>
2026-01-07 22:29:41 -0300

    asda

--- script.js
+++ script.js
@@ -42,7 +42,7 @@ const FALLBACK_DB = {
     "admin": { "email": "admin@paseos.com", "password": "admin123" }
 };
 const DB_URL = 'paseoDogDB.json';
-let TRAINER_PHONE = "096921960"; // ✅ NÚMERO DEL ADMINISTRADOR (público)
+let TRAINER_PHONE = "59896921960"; // ✅ NÚMERO DEL ADMINISTRADOR (público)
 let ADMIN_USER = { email: 'admin@paseos.com', password: 'admin123' };
 let EXAMPLE_DOGS = [];
 let REAL_DOGS = [];
